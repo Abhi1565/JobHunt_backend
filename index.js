58566e5e26184ee67af741eaff238c3f163846a9
Abhijit Palve <palweabhijeet@gmail.com>
2025-07-16 18:55:17 +0530

    Adding deployed frontend url

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-   origin:'http://localhost:5173',
-   credentials:true
+   origin: [
+     'http://localhost:5173',
+     'https://jobhunt-site-6d3w.onrender.com'
+   ],
+   credentials: true
 }
 app.use(cors(corsOptions))
 
